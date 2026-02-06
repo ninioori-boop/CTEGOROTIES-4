57cd4d8e7a8a6f0b5181e4bcf3bc648dca2d45b9
ninioori-boop <ninioori@gmail.com>
2026-02-06 15:30:53 +0200

    header path first then content - refunds fix

--- push-short-path.bat
+++ push-short-path.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul
-cd /d "%~dp0"
+cd /d "C:\Users\ninio\OneDrive\B7A3~1\CTEGOROTIES-4-main"
 git add .
 git commit -m "קודם נתיב כותרות אחר כך תוכן - החזרים נכונים בדוח"
 git push origin main
